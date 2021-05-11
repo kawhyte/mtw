61d2f72b147f71a09ad57d961d63e9d0b811f432
Kenny Whyte <kawhyte@gmail.com>
2021-05-11 15:11:16 -0700

    updated autofixer

--- components/navbar.js
+++ components/navbar.js
@@ -23,7 +23,7 @@ function Navbar() {
 									aria-label='toggle menu'>
 									<svg viewBox='0 0 24 24' className='w-6 h-6 fill-current'>
 										<path
-											fill-rule='evenodd'
+											fillRule='evenodd'
 											d='M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z'></path>
 									</svg>
 								</button>

a867a42cebafaed9366ffb9a12ac126bdefbd458
Kenny Whyte <kawhyte@gmail.com>
2021-05-11 15:28:50 -0700

    kee

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,18 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
+
 import Hero from '../components/main-hero'
 import Section from '../components/section'
 import Welcome from '../components/welcome'
-import Intro from '../components/intro'
 import Layout from '../components/layout'
-import { getAllPostsForHome } from '../lib/api'
+
 import Head from 'next/head'
 import { CMS_NAME } from '../lib/constants'
 
-export default function Index({ allPosts, preview }) {
-  const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
+export default function Index() {
+
   return (
     <>
-      <Layout preview={preview}>
+      <Layout >
         <Head>
           <title>Meet the Whytes by {CMS_NAME}</title>
         </Head>
@@ -29,10 +26,3 @@ export default function Index({ allPosts, preview }) {
   )
 }
 
-export async function getStaticProps({ preview = false }) {
-  const allPosts = await getAllPostsForHome(preview)
-  return {
-    props: { allPosts, preview },
-    revalidate: 1
-  }
-}

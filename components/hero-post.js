414b92b5ca96915885b49f54dc9f738d03fd9673
Kenny Whyte <kawhyte@gmail.com>
2021-05-13 15:13:41 -0700

    hero

--- components/hero-post.js
+++ components/hero-post.js
@@ -16,9 +16,14 @@ export default function HeroPost({
 		<section className=''>
 			<div className='flex flex-col justify-start items-center'>
 				<div className='mb-8 md:mb-10 relative'>
-					<span class='px-10 py-2 mx-2 my-2  text-base rounded-full text-green-600  bg-green-200 absolute '>
-						<StarRating />
+					<span class='flex  flex-row-reverse px-3 py-2 mx-2 my-2  text-base rounded-full text-green-600  bg-green-200 absolute '>
+						<p className=' ml-2 '> MTW Award</p>
+
+						<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+						<path fillRule="evenodd" d="M2.166 4.999A11.954 11.954 0 0010 1.944 11.954 11.954 0 0017.834 5c.11.65.166 1.32.166 2.001 0 5.225-3.34 9.67-8 11.317C5.34 16.67 2 12.225 2 7c0-.682.057-1.35.166-2.001zm11.541 3.708a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
+					  </svg>
 					</span>
+
 					<div className=' absolute bottom-0 right-0  mx-2 my-1 '>
 						<h3 className='mb-2 md:mb-8 text-xl md:text-4xl font-bold tracking-tighter leading-tight bg-white px-3 py-2'>
 							<Link as={`/posts/${slug}`} href='/posts/[slug]'>

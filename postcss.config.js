ba682633aa9cdc645d6be129ca531de5e455a7c7
Kenny Whyte <kawhyte@gmail.com>
2021-05-12 19:32:03 -0700

    Postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,8 @@
 module.exports = {
-  plugins: [
-    'tailwindcss',
-    ...(process.env.NODE_ENV === 'production'
-      ? [ purgecss
-          // [
-          //   '@fullhuman/postcss-purgecss',
-          //   {
-          //     content: [
-          //       './pages/**/*.{js,jsx,ts,tsx}',
-          //       './components/**/*.{js,jsx,ts,tsx}',
-          //     ],
-          //     defaultExtractor: (content) =>
-          //       content.match(/[\w-/:]+(?<!:)/g) || [],
-          //   },
-          // ],
-        ]
-      : []),
-    'postcss-preset-env',
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 }
 // module.exports = {
 //   plugins: [

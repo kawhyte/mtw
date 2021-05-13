bd6f895ee4c64659fe69c0ef224f19490151a1e0
Kenny Whyte <kawhyte@gmail.com>
2021-05-13 09:54:57 -0700

    page

--- components/navbar.js
+++ components/navbar.js
@@ -8,12 +8,15 @@ function Navbar({ fixed }) {
 		<nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
+           
+		  <Link href='/'>
+		  <a
               className="text-base font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
               href="#pablo"
             >
               Meet The Whytes
             </a>
+		</Link>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -39,24 +42,17 @@ function Navbar({ fixed }) {
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   href="#pablo"
                 >
-                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
+                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Home</span>
                 </a>
               </li>
               <li className="nav-item">
+			  <Link href='/allposts'> 
                 <a
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
                   href="#pablo"
                 >
-                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Tweet</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
-                >
-                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Pin</span>
-                </a>
+                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">All Posts</span>
+                </a></Link>
               </li>
             </ul>
           </div>
--- components/post-header.js
+++ components/post-header.js
@@ -11,27 +11,66 @@ export default function PostHeader({ title, coverImage, date, author }) {
 			<div className=' block mb-2 text-lg'>
 				<StarRating />
 			</div>
-      <div className='max-w-2xl mx-auto'>
-      <div className=' text-gray-500 block  md:hidden mb-2 text-lg'>
-      Review date:	<Date dateString={date} />
-      </div>
-    </div>
-      <div className='max-w-2xl mx-auto'>
-      <div className=' text-gray-500 block  md:hidden mb-6 text-lg'>
-     Location:	San Francisco
-      </div>
-    </div>
-			<div className='block md:mb-12'>
-				<Avatar name={author?.name} picture={author?.picture} />
+			<div className='max-w-2xl flex flex-row  '>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					className='h-6 w-6'
+					fill='none'
+					viewBox='0 0 24 24'
+					stroke='currentColor'>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						strokeWidth={2}
+						d='M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z'
+					/>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						strokeWidth={2}
+						d='M15 11a3 3 0 11-6 0 3 3 0 016 0z'
+					/>
+				</svg>
+				<div className=' text-gray-500 block   mb-2 ml-2 text-lg'>
+					San Francisco
+				</div>
 			</div>
-			<div className=' hidden  md:block mb-6 text-lg'>
-				<Date dateString={date} />
+			<div className='max-w-2xl flex flex-row '>
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+    </svg>
+
+    
+				<div className=' text-gray-500 block ml-2   mb-2  text-lg'>
+					Visit Hotel Website 
+				</div>
+			</div>
+			<div className='max-w-2xl flex flex-row '>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					className='h-6 w-6'
+					fill='none'
+					viewBox='0 0 24 24'
+					stroke='currentColor'>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						strokeWidth={2}
+						d='M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z'
+					/>
+				</svg>
+
+				<div className=' text-gray-500 block ml-2   mb-6 md:mb-12 text-lg'>
+					<Date dateString={date} />
+				</div>
 			</div>
 
 			<div className='mb-8 md:mb-16 -mx-5 sm:mx-0'>
 				<CoverImage title={title} imageObject={coverImage} url={coverImage} />
 			</div>
-		
 		</>
 	);
 }
+// <div className='block mb-6 md:mb-12'>
+// 	<Avatar name={author?.name} picture={author?.picture} />
+// </div>

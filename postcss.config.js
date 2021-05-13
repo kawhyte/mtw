a3bdd3a242331dfa14c73e724da5dfed428d452b
Kenny Whyte <kawhyte@gmail.com>
2021-05-12 19:26:18 -0700

    testing css

--- postcss.config.js
+++ postcss.config.js
@@ -2,20 +2,41 @@ module.exports = {
   plugins: [
     'tailwindcss',
     ...(process.env.NODE_ENV === 'production'
-      ? [
-          [
-            '@fullhuman/postcss-purgecss',
-            {
-              content: [
-                './pages/**/*.{js,jsx,ts,tsx}',
-                './components/**/*.{js,jsx,ts,tsx}',
-              ],
-              defaultExtractor: (content) =>
-                content.match(/[\w-/:]+(?<!:)/g) || [],
-            },
-          ],
+      ? [ purgecss
+          // [
+          //   '@fullhuman/postcss-purgecss',
+          //   {
+          //     content: [
+          //       './pages/**/*.{js,jsx,ts,tsx}',
+          //       './components/**/*.{js,jsx,ts,tsx}',
+          //     ],
+          //     defaultExtractor: (content) =>
+          //       content.match(/[\w-/:]+(?<!:)/g) || [],
+          //   },
+          // ],
         ]
       : []),
     'postcss-preset-env',
   ],
 }
+// module.exports = {
+//   plugins: [
+//     'tailwindcss',
+//     ...(process.env.NODE_ENV === 'production'
+//       ? [
+//           [
+//             '@fullhuman/postcss-purgecss',
+//             {
+//               content: [
+//                 './pages/**/*.{js,jsx,ts,tsx}',
+//                 './components/**/*.{js,jsx,ts,tsx}',
+//               ],
+//               defaultExtractor: (content) =>
+//                 content.match(/[\w-/:]+(?<!:)/g) || [],
+//             },
+//           ],
+//         ]
+//       : []),
+//     'postcss-preset-env',
+//   ],
+// }
